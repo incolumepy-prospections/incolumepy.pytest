280d07e59d8eaf7823d8212e535b6663257aeadf
Britodfbr <britodfbr@gmail.com>
2021-10-07 15:40:45 -0300

    Exemplos de teste unitário com pytest

--- src/incolumepy/example_pytest/__init__.py
+++ src/incolumepy/example_pytest/__init__.py
@@ -1,1 +1,20 @@
-__version__ = '0.1.0'
+from pathlib import Path
+from typing import Tuple
+import re
+import toml
+
+path = list(Path(__file__).parent.parts)
+__path__ = str(Path(*path[:path.index('incolumepy.pytest') + 1]))
+__version__ = toml.load(Path(__path__).joinpath('pyproject.toml'))['tool']['poetry']['version']   # '0.1.0'
+
+
+class VersionError(ValueError):
+    pass
+
+
+def parser_version(s: str) -> Tuple[int, int]:
+    # result = tuple(int(x) for x in s.split('.'))
+    result = tuple(int(x) for x in re.split(r'[.-]', s))
+    if len(result) > 3 or len(result) < 2:
+        raise VersionError(f'Expected #.#[.#] bug got {s!r}')
+    return result
--- tests/test_incolumepy_pytest.py
+++ tests/test_incolumepy_pytest.py
@@ -1,5 +1,70 @@
-from src.incolumepy.example_pytest import __version__
+import pytest
+import re
+from src.incolumepy.example_pytest import __version__, parser_version, VersionError
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    # assert __version__ == "0.1.0"
+    assert re.fullmatch(r'\d.\d.\d(-.*.?\d)?', __version__, flags=re.I)
+
+
+@pytest.mark.parametrize(
+    ("strinput", "expected"),
+    (
+        ("2.7", (2, 7)),
+        ("3.9", (3, 9)),
+        ("3.10", (3, 10)),
+    ),
+)
+def test_parser_version_minors(strinput, expected):
+    assert parser_version(strinput) == expected
+
+
+@pytest.mark.parametrize(
+    ("strinput", "expected"),
+    (
+        ("2.7.6", (2, 7, 6)),
+        ("3.8.16", (3, 8, 16)),
+        ("3.9.7", (3, 9, 7)),
+        ("3.10.1", (3, 10, 1)),
+    ),
+)
+def test_parser_version_patches(strinput, expected):
+    assert parser_version(strinput) == expected
+
+
+def test_parser_version_not_number():
+    with pytest.raises(ValueError):
+        parser_version('3.n')
+
+
+@pytest.mark.parametrize(
+    ("strinput", "expected"),
+    (
+        ('3', 'Expected #.#[.#] bug got "3"'),
+        ('3.1.1.1', 'Expected #.#[.#] bug got "3.1.1.1"'),
+    )
+)
+def test_parser_version_failure_segment_count(strinput, expected):
+    with pytest.raises(VersionError) as e:
+        parser_version(strinput)
+    msg, = e.value.args
+    assert msg == f'Expected #.#[.#] bug got {strinput!r}'
+
+
+@pytest.mark.parametrize(
+    ("strinput", "expected"),
+    (
+        ('3.1.0-dev0', f"invalid literal for int() with base 10: 'dev0'"),
+        ('3.1.1-dev.0', f"invalid literal for int() with base 10: 'dev'"),
+        ('3.10.0-alpha.0', f"invalid literal for int() with base 10: 'alpha'"),
+        ('3.11.0-a.1', f"invalid literal for int() with base 10: 'a'"),
+        ('3.11.0.a.2', f"invalid literal for int() with base 10: 'a'"),
+    )
+)
+def test_parser_version_failure_segment_unstable(strinput, expected):
+    with pytest.raises(ValueError) as e:
+        parser_version(strinput)
+    msg, = e.value.args
+    assert msg == expected
+
